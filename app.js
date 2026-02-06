c1e5c2b58d1ce5ac9127f667d096fec068e74fcb
ElioSpena <elio.spena.94@gmail.com>
2026-02-06 14:05:40 +0100

    feat: implementato formattazione date con luxon

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ import express from "express";
 import connection from "./database/connectionDb.js";
 import handleError from "./middlewares/handleError.js";
 import moviesRouter from "./routers/moviesRouter.js";
+import routeNotFound from "./middlewares/routeNotFound.js";
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.use("/movies", moviesRouter);
+
+app.use(routeNotFound);
 
 app.use(handleError);
 
--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,14 +1,29 @@
 import connection from "../database/connectionDb.js";
+import { DateTime } from "luxon";
+
+//INDEX
 
 function index(req, res, next) {
   const moviesQuery = `SELECT * FROM movies`;
   connection.query(moviesQuery, (err, results) => {
     if (err) return next(err);
-    return res.json({
-      results,
+
+    const movies = results.map((r) => {
+      const date = r.created_at;
+      const update = r.updateDate_at;
+      const formattedDate = DateTime.fromObject(date);
+      const formattedupdate = DateTime.fromObject(update);
+      return {
+        ...r,
+        created_at: formattedDate.toLocaleString(),
+        updated_at: formattedupdate.toLocaleString(),
+      };
     });
+    return res.json(movies);
   });
 }
+
+//SHOW
 
 function show(req, res, next) {
   const id = req.params.id;
