e236ad3a36073df81462523bc660a4bf1c2b2166
ElioSpena <elio.spena.94@gmail.com>
2026-02-10 11:46:07 +0100

    feat: implementato middleware cors ed effettuata chiamata su postman

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ import connection from "./database/connectionDb.js";
 import handleError from "./middlewares/handleError.js";
 import moviesRouter from "./routers/moviesRouter.js";
 import routeNotFound from "./middlewares/routeNotFound.js";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use(express.static("public"));
 
